3584ee2c3b36c73752c30aa245761543e2a4c969
mehtadarshan <darshanmehta786@yahoo.in>
2017-04-14 18:04:24 -0400

    database

---
+++ model/database.php
@@ -0,0 +1,24 @@
+<?php
+
+function dbConnect(){
+		$dbconn = pg_connect("host=mcsdb.utm.utoronto.ca dbname=mehtada3_309 user=mehtada3 password=05270");
+		return $dbconn;
+}
+
+function login($dbconn, $username, $passwd){
+
+	$result = pg_prepare($dbconn, "", 'SELECT * FROM voters1 WHERE username=$username and paswd=$passwd');
+
+	## check if database was able to prepare it
+	if (!($result)){
+		pg_close($dbconn);
+		break;
+	}
+
+	$result = pg_execute($dbconn, "", array());
+
+	return $result;
+
+}
+
+?>
--- model/schema.sql
+++ model/schema.sql
@@ -3,11 +3,12 @@ DROP TABLE IF EXISTS voters1 CASCADE;
 -- Voter Details
 CREATE TABLE voters1 (
 username VARCHAR(50) PRIMARY KEY,
-passwd VARCHAR(50) NOT NULL
+passwd VARCHAR(50) NOT NULL,
+vote VARCHAR(50) NOT NULL
 );
 
 INSERT INTO voters1 (username, passwd) VALUES
-('darshan', '1234'),
-('ramy', '1234'),
-('mo', '1234'),
-('raj', '1234');
+('darshan', '1234', 'no'),
+('ramy', '1234', 'no'),
+('mo', '1234', 'no'),
+('raj', '1234','no');
