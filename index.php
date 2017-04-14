3d73be08fd9e810b84268cb8d9d4f7e214750f36
mehtadarshan <darshanmehta786@yahoo.in>
2017-04-14 16:28:23 -0400

    basic login

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    require_once "model/database.php";
+
+    session_save_path("sess");
+    session_start();
+
+    ini_set('display_errors', 'Off');
+
+    $errors=array();
+
+    $view="";
+
+    if(!isset($_SESSION['state'])){
+        $_SESSION['state']='login';
+    }
+
+    switch($_SESSION['state']){
+        case "login":
+        $view="login.php";
+        	break;
+
+    }
+
+    require_once "view/$view";
+?>
