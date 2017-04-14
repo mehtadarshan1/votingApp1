86816fe5bfff96dd6a168320742d17ea9b1312eb
mehtadarshan <darshanmehta786@yahoo.in>
2017-04-14 17:33:38 -0400

    d

--- model/schema.sql
+++ model/schema.sql
@@ -6,7 +6,7 @@ username VARCHAR(50) PRIMARY KEY,
 passwd VARCHAR(50) NOT NULL
 );
 
-INSERT INTO voters1 VALUES
+INSERT INTO voters1 (username, passwd) VALUES
 ("darshan", "1234"),
 ("raj", "1234"),
 ("ramy", "1234"),
