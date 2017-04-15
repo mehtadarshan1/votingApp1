6e0def0682a4a4fef4d94436718d6628c2d335bd
mehtadarshan <darshanmehta786@yahoo.in>
2017-04-15 13:54:02 -0400

    vote

--- view/votepage.php
+++ view/votepage.php
@@ -1,1 +1,19 @@
-voting app
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <!-- <link rel="stylesheet" type="text/css" href="style.css" /> -->
+        <title>Voting Page</title>
+    </head>
+    <body>
+
+        <?php echo(view_errors($errors)); ?>
+        <?php echo(view_errors($debug)); ?>
+
+        <footer>
+        <h1>Logout</h1>
+            <form method='post'>
+            	<input type="submit" name="logout" value="logout" />
+            </form>
+        </footer>
+    </body>
+</html>
