3ed528c2b62920f6115dea76ecda1dcbc1c38d86
mehtadarshan <darshanmehta786@yahoo.in>
2017-04-15 21:02:01 -0400

    s

--- view/votepage.php
+++ view/votepage.php
@@ -1,8 +1,8 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <script language="javascript" src="view/script.js"></script>
         <script language="javascript" src="view/jquery.php"></script>
+        <script language="javascript" src="view/script.js"></script>
         <link rel="stylesheet" type="text/css" href="view/style.css">
         <title>Voting Page</title>
     </head>
